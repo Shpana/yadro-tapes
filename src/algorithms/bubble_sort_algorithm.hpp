24fa7cb75cc26c70e257c46f743aafb86a03ea34
Shpana <romankozell@yandex.ru>
2025-05-02 01:36:21 +0300

    remove intermediate tape for bubble sort

--- src/algorithms/bubble_sort_algorithm.hpp
+++ src/algorithms/bubble_sort_algorithm.hpp
@@ -11,35 +11,28 @@ public:
 
   void run() override {
     size_t size = _input_tape->get_size();
-    {
-      auto temp_tape =
-          std::make_unique<FileBasedTapeStorage>("../tests/tmp/temp.txt", size);
-      CopyAlgorithm(std::move(_input_tape), std::move(temp_tape), {})
-          .run();
+
+    while (_input_tape->get_head_position() < size) {
+      _output_tape->write(_input_tape->read());
+      if (_input_tape->get_head_position() == size - 1)
+        break;
+      _input_tape->move_forward();
+      _output_tape->move_forward();
     }
-    {
-      auto temp_tape =
-          std::make_unique<FileBasedTapeStorage>("../tests/tmp/temp.txt", size);
-      for (size_t i = 0; i < size - 1; ++i) {
-        temp_tape->reset();
-        for (size_t j = 0; j < size - i - 1; ++j) {
-          auto lhs = temp_tape->read();
-          temp_tape->move_forward();
-          auto rhs = temp_tape->read();
-          if (lhs > rhs) {
-            temp_tape->write(lhs);
-            temp_tape->move_back();
-            temp_tape->write(rhs);
-            temp_tape->move_forward();
-          }
+
+    for (size_t i = 0; i < size - 1; ++i) {
+      _output_tape->reset();
+      for (size_t j = 0; j < size - i - 1; ++j) {
+        auto lhs = _output_tape->read();
+        _output_tape->move_forward();
+        auto rhs = _output_tape->read();
+        if (lhs > rhs) {
+          _output_tape->write(lhs);
+          _output_tape->move_back();
+          _output_tape->write(rhs);
+          _output_tape->move_forward();
         }
       }
-    }
-    {
-      auto temp_tape =
-          std::make_unique<FileBasedTapeStorage>("../tests/tmp/temp.txt", size);
-      CopyAlgorithm(std::move(temp_tape), std::move(_output_tape), {})
-          .run();
     }
   }
 };
