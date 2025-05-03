d6396379022b0c7ed0a4c0c94e388efd90156266
Shpana <romankozell@yandex.ru>
2025-05-03 18:51:01 +0300

    extend sorting tests

--- yadro_tapes_tests/src/file_based_tape_tests.cpp
+++ yadro_tapes_tests/src/file_based_tape_tests.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 
+#include "utils/temp_tape_files.hpp"
+
 #include "utils/static_tape_files.hpp"
 
 TEST(file_based_tape_tests, creation) {
@@ -16,4 +18,22 @@ TEST(file_based_tape_tests, creation_file_not_found) {
 TEST(file_based_tape_tests, reading) {
   auto tape = load_static_tape("reading.dat", 1);
   ASSERT_EQ(tape->read(), 42);
+}
+
+TEST(file_based_tape_tests, writing) {
+  auto tape = create_temp_tape("1.dat", 1);
+  tape->write(24);
+  ASSERT_EQ(tape->read(), 24);
+  tape->write(42);
+  ASSERT_EQ(tape->read(), 42);
+}
+
+TEST(file_based_tape_tests, moving) {
+  auto tape = load_static_tape("100_inverse.dat", 100);
+  ASSERT_EQ(tape->read(), 99);
+  tape->move_forward();
+  tape->move_forward();
+  ASSERT_EQ(tape->read(), 97);
+  tape->move_back();
+  ASSERT_EQ(tape->read(), 98);
 }
--- yadro_tapes_tests/src/sorting_tests.cpp
+++ yadro_tapes_tests/src/sorting_tests.cpp
@@ -42,13 +42,39 @@ TEST(sorting_tests, merge_sort) {
         .run();
   }
   {
-    auto tape = load_temp_tape("output.dat", size);
-    auto buffer = read_many(std::move(tape), size);
+    std::unique_ptr<Tape> tape = load_temp_tape("output.dat", size);
+    auto buffer = read_many(tape, size);
 
     std::vector<Tape::Data> expected_buffer(size, 0);
     for (size_t i = 0; i < size; ++i)
       expected_buffer[i] = static_cast<Tape::Data>(i);
 
+    ASSERT_EQ(buffer, expected_buffer);
+  }
+}
+
+TEST(sorting_tests, merge_sort_little) {
+  size_t size = 5;
+  size_t memory_limit = 2;
+  {
+    std::unique_ptr<Tape> input_tape = create_temp_tape("input.dat", size);
+    std::unique_ptr<Tape> output_tape = create_temp_tape("output.dat", size);
+    std::array<std::unique_ptr<Tape>, 2> extra_tapes = {
+        create_temp_tape("1.dat", size),
+        create_temp_tape("2.dat", size)};
+    auto specs = MemoryLimitSpec(memory_limit);
+
+    std::vector<Tape::Data> values = {4, 2, 1, 3, 0};
+    write_many(input_tape, values);
+    reset(input_tape);
+
+    MergeSortAlgorithm(std::move(input_tape), std::move(output_tape), extra_tapes, specs)
+        .run();
+  }
+  {
+    std::unique_ptr<Tape> tape = load_temp_tape("output.dat", size);
+    auto buffer = read_many(tape, size);
+    std::vector<Tape::Data> expected_buffer = {0, 1, 2, 3, 4};
     ASSERT_EQ(buffer, expected_buffer);
   }
 }
